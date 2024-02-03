1b8e8a832c13401232fbd881a70d17569edf45ed
DeftSlyNinja <christopher_masson@hotmail.com>
2024-02-03 10:39:01 -0800

    Edited HTML & JS and added example for CSS

--- main.js
+++ main.js
@@ -7,9 +7,9 @@ let GameObject = function (pTitle, pGenre, pLink, pPriority) {
     this.prio = pPriority;
 }
 
-GameArray.push(new GameObject("Persona 3", "RPG", "https://www.google.com", 1));
-GameArray.push(new GameObject("Call of Duty", "FPS", "www.google.com", 2));
-GameArray.push(new GameObject("XCOM", "Strategy", "www.google.com", 3));
+GameArray.push(new GameObject("Persona 3", "RPG", "https://store.steampowered.com/app/2161700/Persona_3_Reload/", 1));
+GameArray.push(new GameObject("Call of Duty", "FPS", "https://store.steampowered.com/app/1938090/Call_of_Duty/", 2));
+GameArray.push(new GameObject("XCOM", "Strategy", "https://store.steampowered.com/app/200510/XCOM_Enemy_Unknown/", 3));
 
 document.addEventListener("DOMContentLoaded", function (event) {
 
@@ -18,7 +18,8 @@ document.addEventListener("DOMContentLoaded", function (event) {
     document.getElementById("addButton").addEventListener("click", function () {
         GameArray.push(new GameObject(document.getElementById("title").value, selectedType, document.getElementById("link").value,
             document.getElementById("priority").value));
-
+        
+        alert(document.getElementById("title").value + " has been added to the backlog!")
         document.getElementById("title").value = "";
         document.getElementById("link").value = "";
         document.getElementById("priority").value = "";
@@ -50,7 +51,7 @@ function createBacklog() {
         let link = document.createElement("a");
         link.href = element.link;
         link.target = "_blank";
-        link.innerHTML = element.prio + "-  " + element.title + " (" + element.genre + ")";
+        link.innerHTML = element.prio + " -  " + element.title + " (" + element.genre + ")";
         li.appendChild(link);
         backlogUL.appendChild(li);
     })
