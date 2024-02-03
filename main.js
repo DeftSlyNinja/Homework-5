de269cc295ffc9b6279aaa2287d6a4f7afc20649
DeftSlyNinja <christopher_masson@hotmail.com>
2024-02-02 17:58:15 -0800

    Added JS

--- main.js
+++ main.js
@@ -1,53 +1,92 @@
-let NoteArray = [];
+let GameArray = [];
 
-let NoteObject = function (pData, pType, pPriority) {
-    this.data = pData;
-    this.type = pType;
-    this.priority = pPriority;
+let GameObject = function (pTitle, pGenre, pLink, pPriority) {
+    this.title = pTitle;
+    this.genre = pGenre;
+    this.link = pLink;
+    this.prio = pPriority;
 }
 
-NoteArray.push ( new NoteObject("Eat Lunch", "Home", 1)  );
-NoteArray.push ( new NoteObject("Do 209 HW", "School", 2)  );
-NoteArray.push ( new NoteObject("Watch Dune", "Home", 3)  );
+GameArray.push(new GameObject("Persona 3", "RPG", "https://www.google.com", 1));
+GameArray.push(new GameObject("Call of Duty", "FPS", "www.google.com", 2));
+GameArray.push(new GameObject("XCOM", "Strategy", "www.google.com", 3));
 
-let selectedType = "";
-
-// code runs immediately
-//================================================================
-
-// runs  when dom is loaded
 document.addEventListener("DOMContentLoaded", function (event) {
 
-    createList();
+    createBacklog();
 
-    document.getElementById("buttonAdd").addEventListener("click", function () {
-        NoteArray.push ( new NoteObject(document.getElementById("dataInput").value, selectedType,
-        document.getElementById("priorityInput").value ) );
-        
-        document.getElementById("dataInput").value = "";
-        document.getElementById("priorityInput").value = "";
+    document.getElementById("addButton").addEventListener("click", function () {
+        GameArray.push(new GameObject(document.getElementById("title").value, selectedType, document.getElementById("link").value,
+            document.getElementById("priority").value));
 
-        createList();
+        document.getElementById("title").value = "";
+        document.getElementById("link").value = "";
+        document.getElementById("priority").value = "";
+
+        createBacklog();
+    })
+
+    $(document).bind("change", "#genre", function (event, ui) {
+        selectedType = document.getElementById("genre").value;
     });
 
-    $(document).bind("change", "#select-type", function (event, ui) {
-        selectedType = document.getElementById("select-type").value;
+    $(document).bind("change", "#selectFilter", function (event, ui) {
+        filteredGenre = document.getElementById("selectFilter").value;
     });
 
-});
+    document.getElementById("filterButton").addEventListener("click", createFilteredList);
 
+    document.getElementById("searchButton").addEventListener("click", searchBacklog);
+})
 
-//======================================
-// function defintions
-function createList() {
-    // clear prior data
-    var myul = document.getElementById("myul");
-    myul.innerHTML = "";
+// Functions Below
 
-    NoteArray.forEach(function (element,) {   // use handy array forEach method
-        var li = document.createElement('li');
-          // added data-role="listview" to the ul in the html
-        li.innerHTML = element.priority + ":  " + element.type + "   " + element.data;
-        myul.appendChild(li);
-    });
-};
+function createBacklog() {
+    let backlogUL = document.getElementById("backlogUL");
+    backlogUL.innerHTML = "";
+
+    GameArray.forEach(function (element,) {
+        let li = document.createElement("li");
+        let link = document.createElement("a");
+        link.href = element.link;
+        link.target = "_blank";
+        link.innerHTML = element.prio + "-  " + element.title + " (" + element.genre + ")";
+        li.appendChild(link);
+        backlogUL.appendChild(li);
+    })
+}
+
+function createFilteredList() {
+    let filteredUL = document.getElementById("filteredUL");
+    filteredUL.innerHTML = "";
+
+    GameArray.forEach(function (game,) {
+        if (game.genre == document.getElementById("selectFilter").value) {
+            let li = document.createElement("li");
+            let link = document.createElement("a");
+            link.href = game.link;
+            link.target = "_blank";
+            link.innerHTML = game.prio + "-  " + game.title + " (" + game.genre + ")";
+            li.appendChild(link);
+            filteredUL.appendChild(li);
+        }
+    })
+
+}
+
+function searchBacklog() {
+    let searchEntry = document.getElementById("searchEntry");
+
+    GameArray.forEach(function (game,) {
+        if (game.title.toLowerCase() == searchEntry.value.toLowerCase()) {
+            let li = document.createElement("li");
+            let link = document.createElement("a");
+            link.href = game.link;
+            link.target = "_blank";
+            link.innerHTML = game.prio + "-  " + game.title + " (" + game.genre + ")";
+            li.appendChild(link);
+            filteredUL.appendChild(li);
+        }})
+    
+    searchEntry.innerHTML = "";
+}
